b02354ed649308ec338772e67074d37d5a1f3635
ikumi-tajima <ikumi.tajima@fiertecorp.com>
2019-12-05 16:09:55 +0900

    [update] AttrDict 辞書の中のリストの中の辞書も漏れなくAttrDictに変換する

--- fmodules/dict_wrapper.py
+++ fmodules/dict_wrapper.py
@@ -1,7 +1,14 @@
 class AttrDict(dict):
     def __init__(self, *args, **kwargs):
+        def SearchDictAndReplace(node):
+            if type(node) is dict:
+                return AttrDict(node)
+            if type(node) is list:
+                for i, elm in enumerate(node):
+                    node[i] = SearchDictAndReplace(elm)
+            return node
+
         super().__init__(*args, **kwargs)
         for key, val in self.items():
-            if type(val) is dict:
-                self[key] = AttrDict(val)
+            self[key] = SearchDictAndReplace(val)
         self.__dict__ = self
