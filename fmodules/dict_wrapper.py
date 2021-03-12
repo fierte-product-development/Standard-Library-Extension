b9c456a69618510aad9d965b3ee125f2c9fd40e9
ikumirai <tajima_ikumi@me.com>
2021-03-12 16:37:09 +0900

    [update] AttrDict 存在しない属性にアクセスした際の例外にメッセージを追加

--- fmodules/dict_wrapper.py
+++ fmodules/dict_wrapper.py
@@ -2,21 +2,20 @@ from typing import Any
 
 
 class AttrDict(dict):
-    """This is a dict allow their elements to be accessed both as keys and as attributes."""
+    """AttrDict is a dict allow their elements to be accessed both as keys and as attributes"""
 
     def __init__(self, *args, **kwargs):
-        def SearchDictAndReplace(node):
+        def CastDict(node):
             if type(node) is dict:
                 return AttrDict(node)
             if type(node) is list:
-                for i, elm in enumerate(node):
-                    node[i] = SearchDictAndReplace(elm)
+                node = [CastDict(elm) for elm in node]
             return node
 
         super().__init__(*args, **kwargs)
         for key, val in self.items():
-            self[key] = SearchDictAndReplace(val)
+            self[key] = CastDict(val)
         self.__dict__ = self
 
     def __getattr__(self, name) -> Any:
-        raise AttributeError
+        raise AttributeError(f"This 'AttrDict' has no attribute '{name}'")
