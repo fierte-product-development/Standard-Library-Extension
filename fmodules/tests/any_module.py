89d40c739c19afbbc7c4a83fb5b643c9504e1bdc
ikumirai <tajima_ikumi@me.com>
2021-07-01 14:19:41 +0900

    [update] 新仕様版getLoggerのテストを追加

---
+++ fmodules/tests/any_module.py
@@ -0,0 +1,4 @@
+from logging import Logger
+from ..logging_wrappers import getLogger
+
+logger: Logger = getLogger("child")
--- fmodules/tests/test_logging_wrappers.py
+++ fmodules/tests/test_logging_wrappers.py
@@ -1,6 +1,26 @@
-from .. import logging_wrappers
+import pytest
+from typing import Optional
+from logging import Logger, FileHandler
+from pathlib import Path
+from ..logging_wrappers import getLogger
+
+from . import any_module
 
 
 class Test_getLogger:
-    def test_test(self):
-        assert True
+    def FindFileHandler(self, l: Logger) -> Optional[FileHandler]:
+        for h in l.handlers:
+            if isinstance(h, FileHandler):
+                return h
+
+    @pytest.mark.order(1)
+    def test_SettingsOfChildLoggerWillNotChange_RootIsFalse(self):
+        my_logger = getLogger("parent_a", Path("a"))
+        assert self.FindFileHandler(my_logger)
+        assert not self.FindFileHandler(any_module.logger)
+
+    @pytest.mark.order(2)
+    def test_SettingsOfChildLoggerWillChange_RootIsTrue(self):
+        my_logger = getLogger("parent_b", Path("b"), root=True)
+        assert self.FindFileHandler(my_logger)
+        assert self.FindFileHandler(any_module.logger)
