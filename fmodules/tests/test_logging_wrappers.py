9d98f2b651d5e0d0441ea46c8a1125ca5727425a
junkmd <jun.komoda@fiertecorp.com>
2022-01-20 18:57:37 +0900

    [update]#6 fixture name and test method name

--- fmodules/tests/test_logging_wrappers.py
+++ fmodules/tests/test_logging_wrappers.py
@@ -9,7 +9,7 @@ from . import any_module, raise_zero_div
 
 
 @pytest.fixture
-def created_time(mocker) -> None:
+def fixed_time(mocker) -> None:
     # `logging.time.time()` will be assigned to `LogRecord.created` attribute.
     # Therefore, log messages' asctime will be fixed.
     ctime = 1000000000
@@ -24,40 +24,40 @@ class Test_fFormatter:
             return fFormatter()
 
         @pytest.mark.parametrize("lv_no, lv_name", [(INFO, "INFO"), (WARNING, "WARNING")])
-        def test_OutputMsg_TakesSpecificLevel(self, fmt: fFormatter, created_time, lv_no, lv_name):
+        def test_OutputMsg_TakesSpecificLevel(self, fmt: fFormatter, fixed_time, lv_no, lv_name):
             rec = LogRecord("name", lv_no, "pathname", 100, "msg", None, None, func="func")
-            assert fmt.format(rec) == f"{lv_name:>8} {created_time} [pathname] msg"
+            assert fmt.format(rec) == f"{lv_name:>8} {fixed_time} [pathname] msg"
 
         @pytest.mark.parametrize("lv_no, lv_name", [(DEBUG, "DEBUG"), (ERROR, "ERROR"), (CRITICAL, "CRITICAL")])
-        def test_OutputLineNoAndFuncName_TakesSpecificLevel(self, fmt: fFormatter, created_time, lv_no, lv_name):
+        def test_OutputLineNoAndFuncName_TakesSpecificLevel(self, fmt: fFormatter, fixed_time, lv_no, lv_name):
             rec = LogRecord("name", lv_no, "pathname", 100, "msg", None, None, func="func")
-            assert fmt.format(rec) == f"{lv_name:>8} {created_time} [pathname] msg (100:func)"
+            assert fmt.format(rec) == f"{lv_name:>8} {fixed_time} [pathname] msg (100:func)"
 
-        def test_OutputStackTrace_SInfoIsStr(self, fmt: fFormatter, created_time):
+        def test_OutputStackTrace_StackInfoIsStr(self, fmt: fFormatter, fixed_time):
             rec = LogRecord("name", ERROR, "pathname", 100, "msg", None, None, func="func", sinfo="sinfo")
-            assert fmt.format(rec) == f"   ERROR {created_time} [pathname] msg (100:func) \nsinfo"
+            assert fmt.format(rec) == f"   ERROR {fixed_time} [pathname] msg (100:func) \nsinfo"
 
-        def test_OutputRoot_RootIsNotRecModule(self, fmt: fFormatter, created_time, mocker):
+        def test_OutputRoot_RootIsNotRecModule(self, fmt: fFormatter, fixed_time, mocker):
             mocker.patch.object(logging_wrappers, "_root", new="_root")
             rec = LogRecord("name", INFO, "pathname", 100, "msg", None, None, func="func")
-            assert fmt.format(rec) == f"    INFO {created_time} [_root.pathname] msg"
+            assert fmt.format(rec) == f"    INFO {fixed_time} [_root.pathname] msg"
 
 
 class Test_getLogger:
-    def test_LoggersInOtherModulesWillLogWithMyName_PassedTrueToRoot(self, capfd, created_time):
+    def test_LoggersInOtherModulesWillLogWithMyName_PassedTrueToRoot(self, capfd, fixed_time):
         getLogger(root=True)
         any_module.log("test")
         out, _ = capfd.readouterr()
-        assert out == f"    INFO {created_time} [{Path(__file__).stem}.any_module] test\n"
+        assert out == f"    INFO {fixed_time} [{Path(__file__).stem}.any_module] test\n"
 
-    def test_LoggersInOtherModulesWillLogWithRootName_PassedRootAndName(self, capfd, created_time):
+    def test_LoggersInOtherModulesWillLogWithRootName_PassedRootAndName(self, capfd, fixed_time):
         root_name = "hoge"
         getLogger(root=True, name=root_name)
         any_module.log("test")
         out, _ = capfd.readouterr()
-        assert out == f"    INFO {created_time} [{root_name}.any_module] test\n"
+        assert out == f"    INFO {fixed_time} [{root_name}.any_module] test\n"
 
-    def test_OutputStackTrace_ErrorRaised(self, capfd, mocker, created_time):
+    def test_OutputStackTrace_ErrorRaised(self, capfd, mocker, fixed_time):
         getLogger(root=True)
         fmt_stack = mocker.spy(Formatter, "formatStack")
         with pytest.raises(ZeroDivisionError):
@@ -65,7 +65,7 @@ class Test_getLogger:
         out, err = capfd.readouterr()
         fmt_stack.assert_called()
         expected = (
-            f"   ERROR {created_time} "
+            f"   ERROR {fixed_time} "
             f"[{Path(__file__).stem}.raise_zero_div] zero div (12:raise_zero_div) \n"
             "Stack (most recent call last):\n"
         )
